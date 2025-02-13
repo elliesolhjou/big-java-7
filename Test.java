3ee50da75b7ee36c0dfc26dbeb303ca2c495a88d
Ellie Solhjou <elliesolhjou@gmail.com>
2025-02-13 15:56:26 -0800

    Self Check 6.8

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test{
+//working on nested loop
+    public static void main (String[] args){
+        //outer loop three times
+        // inner loop four times
+        for (int i=0; i<=2; i++){
+            for (int j=0; j<=3; j++){
+                System.out.print("[]");
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
