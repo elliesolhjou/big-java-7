618f1abfd5e964194c30a9750284abd539f66989
Ellie Solhjou <elliesolhjou@gmail.com>
2025-02-13 16:05:50 -0800

    Practicing Random()

--- Test.java
+++ Test.java
@@ -1,14 +1,21 @@
+import java.util.Random;
+
 public class Test{
-//working on nested loop
     public static void main (String[] args){
         //outer loop three times
         // inner loop four times
         for (int i=0; i<=2; i++){
             for (int j=0; j<=3; j++){
                 System.out.print("[]");
-                System.out.println();
+                
             }
             System.out.println();
         }
+
+        //workign on Random
+        Random dice = new Random();
+        System.out.println(dice.nextInt(54709745));
+        System.out.println(dice.nextDouble());
     }
 }
+
